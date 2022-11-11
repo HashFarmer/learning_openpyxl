c2460c63abd6d7e61d05105cc5adcc98b2ded5f7
HashFarmer <84007065+HashFarmer@users.noreply.github.com>
2022-11-11 10:51:33 +0800

    Update 常用操作.py

--- 常用操作.py
+++ 常用操作.py
@@ -11,6 +11,12 @@ wb.close()
 wb = load_workbook('hello_world.xlsx')
 
 
+print(wb.encoding, end='\n\n')  # 获取文档的字符集编码
+print(wb.properties)  # 获取文档的元数据如标题，创建者，创建日期等
+
+
+
+
 ### 关于工作表
 
 #创建新的工作表
@@ -39,6 +45,14 @@ ws.sheet_properties.tabColor = 'ff72BA'
 
 
 
+## 关于 行、列
+
+
+
+
+
+
+
 ### 关于单元格
 
 #选择单个单元格
@@ -49,8 +63,37 @@ ws.cell(1,1)
 ws['A1:B2']
 ws['A1':'B2']
 
+print(ws.values, end='\n\n')  # 生成器对象，将一行单元格作为元组单元--》组成的生成器
+print(list(ws.values), end='\n\n') # 将生成器对象转换为列表数据，列表中是生成器中的所有数据
+
+
 #
 print(ws2['A1'].value)
+
+cell = ws['A1']  # 获取指定位置的单元格对象
+# 单元格列索引
+print(cell.col_idx)
+print(cell.column)
+# 单元格行索引
+print(cell.row)
+# 单元格列名
+print(cell.column_letter)
+# 单元格的坐标
+print(cell.coordinate)
+# 单元格数字类型
+# 默认是
+# n：数值
+# s：字符串
+# d：日期时间
+print(cell.data_type)
+# 单元格编码格式，默认 utf-8
+print(cell.encoding)
+# 是否有样式
+print(cell.has_style)  # 默认样式是 Normal，如果是默认样式，返回False
+# 单元格样式
+print(cell.style)
+# 单元格样式id
+print(cell.style_id)
 
 
 #<generator object Worksheet._cells_by_row>
