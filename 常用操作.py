56554a3f4e9905dc95c06d6d3ec80008b671dfdf
HashFarmer <84007065+HashFarmer@users.noreply.github.com>
2022-11-11 09:56:52 +0800

    Update 常用操作.py

--- 常用操作.py
+++ 常用操作.py
@@ -4,7 +4,7 @@ from openpyxl import Workbook, load_workbook
 
 # 创建、保存一个空白工作簿
 wb = Workbook() 
-wb.save("wb_openpyxl_3.xlsx") #所有针对工作簿的操作在此之前
+wb.save("wb_openpyxl_3.xlsx") #所有针对工作簿的操作在此之前，而且这句是必须，否则前面的操作无法保存
 wb.close()
 
 #加载指定的工作簿
@@ -14,7 +14,10 @@ wb = load_workbook('hello_world.xlsx')
 ### 关于工作表
 
 #创建新的工作表
-sheet_a = wb.create_sheet('123')
+sheet_a = wb.create_sheet('123') #在所有工作表后面
+#
+sheet_b = wb.create_sheet('test',0) #在指定位置创建工作表
+
 #获取所有的工作表名称
 sheet_names = wb.get_sheet_names()
 print(sheet_names)
@@ -26,6 +29,13 @@ ws = wb.get_sheet_by_name(sheet_names[0])
 ws2 = wb2['test']
 # 方法3
 ws3 = wb2.get_sheet_by_name('test')
+# 方法4
+ws =wb2.active # 第一个工作表？
+
+
+# 操作工作表
+# 对sheet页设置一个颜色（16位的RGB颜色）
+ws.sheet_properties.tabColor = 'ff72BA'
 
 
 
@@ -38,6 +48,10 @@ ws.cell(1,1)
 #选择单元格区域, 指针游走方向，从左到右，从上到下
 ws['A1:B2']
 ws['A1':'B2']
+
+#
+print(ws2['A1'].value)
+
 
 #<generator object Worksheet._cells_by_row>
 ws.iter_rows('A1:C2')
